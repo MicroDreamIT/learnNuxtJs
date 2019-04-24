c4d4e505b001a8991710e683a21794bfb1f2a060
MicroDreamIT <shahanur700@gmail.com>
2019-04-24 15:07:23 +0600

    first push

---
+++ pages/about.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        testing about
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "about",
+        head(){
+            return {
+                title:'hello I am about',
+                meta:[
+                    {name:'description', content:'my custom description'}
+                ]
+            }
+        }
+
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <section class="container">
+    <div>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias debitis eveniet facere laborum modi molestias ratione sunt? Atque autem dolores dolorum facere fugit ipsam optio placeat quaerat, reiciendis totam veniam.d
+    </div>
+  </section>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+  components: {
+    Logo
+  },
+    data(){
+      return {
+          posts:[
+              {title:'testing title', description:'testing description'}
+          ]
+      }
+    }
+}
+</script>
